e029c10cf26f71ffed5c0d9985131110ce9aec64
schmeeed <Schmidt5364@gmail.com>
2023-01-31 23:30:58 -0500

    add additional plots to app

--- app.R
+++ app.R
@@ -27,28 +27,7 @@ get_representatives <- function(state_choice) {
   return(reps2)
 }
 
-#### OLD UI ####
-# ui <- fluidPage(
-#   titlePanel("Legislator Stock Performance"),
-# 
-#   sidebarLayout(
-#     sidebarPanel(
-#       selectInput(inputId = "stateinputid",
-#                   label = "State",
-#                   choices = gov_trades %>% arrange(state) %>% select(state) %>% distinct()),
-#       selectInput(inputId = "repsinputid",
-#                   label = "Representative",
-#                   choices = gov_trades %>% arrange(last_name) %>% select(representative) %>% distinct())
-#     ),
-#     mainPanel(
-#       textOutput("trade_count"),
-#       textOutput("bought_count"),
-#       textOutput("sold_count")
-#               )
-#   )
-# )
-
-#### DASHBOARD UI ####
+#### UI ####
 library(shinydashboard)
 ui <- dashboardPage(
   dashboardHeader(title = 'Legislator Stock Performance Dashboard'),
@@ -71,20 +50,30 @@ ui <- dashboardPage(
     tabsetPanel(
       tabPanel("tab panel 1",
                fluidRow(
-                 column(width = 5,
+                 column(width = 4,
                         column(width = 12,
-                               valueBoxOutput("trade_count"),
-                               valueBoxOutput("bought_count"),
-                               valueBoxOutput("sold_count")),
+                               valueBoxOutput("trade_count", width = "100%"),
+                               column(width = 6,
+                                      valueBoxOutput("bought_count", width = "50%")
+                                       ),
+                               column(width = 6,
+                                      valueBoxOutput("sold_count", width = "50%"))
+                               
+
+                               ),
                         column(width = 12,
-                               valueBoxOutput("rep_state", width = "50%"),
-                               valueBoxOutput("rep_party", width = "50%"))
+                               valueBoxOutput("rep_party", width = "100%"))
                         ),
-                 column(width = 7,
+                 column(width = 8,
                         plotOutput("trade_activity")
                         )
                  ),
-               fluidRow()
+               fluidRow(
+                 column(width = 12,
+                        plotOutput("buy_perf"),
+                        plotOutput("sell_perf")
+                        )
+               )
                ),
       tabPanel("tab panel 2")
       )
@@ -101,7 +90,7 @@ server <- function(input, output, session) {
                       choices = get_representatives(input$stateinputid) # custom function: defined in global
                       )
   })
-  # DEEFINE items
+  #### DEFINE ####
     filtered_count <- reactive({
       gov_trades %>%
         filter((state == input$stateinputid) & (representative == input$repsinputid))# input$stateinputid input$repsinputid
@@ -127,18 +116,13 @@ server <- function(input, output, session) {
         filter((state == input$stateinputid) & (representative == input$repsinputid)) %>%
         distinct(party)
     })
-
     
-
-    
-    
-    # DEFINE output items
-    # output$trade_count <- renderText({
-    #   paste("Number of trades: ", nrow(filtered_count()))
-    # })
+    pal <- c("Winner" = "#01FF70",
+             "Loser" = "grey")
+  #### OUTPUTS ####
     output$trade_count <- renderValueBox({
       valueBox(nrow(filtered_count()), 
-               "Trades", icon = icon("exchange"), color = "aqua")
+               "Trades", icon = icon("exchange"), color = "purple")
     })
     output$bought_count <- renderValueBox({
       valueBox(nrow(bought_count()), 
@@ -148,6 +132,7 @@ server <- function(input, output, session) {
       valueBox(nrow(sold_count()), 
                "Sold", icon = icon("paw"), color = "red")
     })
+
     output$rep_state <- renderValueBox({
       valueBox(rep_state(), 
                "State", icon = icon("flag-usa"), color = "yellow")
@@ -162,39 +147,124 @@ server <- function(input, output, session) {
                       "Democrat" = "blue")
       valueBox(party, "Party", icon = icon, color = color)
     })
-    
-    #ifelse(rep_party() == "Republican", as.character(icon("republican")), as.character(icon("democrat")))
-    
-    
-    # output$bought_count <- renderText({
-    #   paste("Bought: ", nrow(bought_count()))
-    # })
-    # output$sold_count <- renderText({
-    #   paste("Sold: ", nrow(sold_count()))
-    # })
-    # output$rep_state <- renderText({
-    #   paste("State: ", rep_state())
-    # })
-    # output$rep_party <- renderText({
-    #   paste("Party: ", rep_party())
-    # })
     output$trade_activity <- renderPlot(
-        gov_trades %>%
+      gov_trades %>%
           filter((state == input$stateinputid) & (representative == input$repsinputid)) %>%
           mutate(transaction_date = as.Date(transaction_date)) %>%
           arrange(transaction_date) %>%
           mutate(quarter_year = format(as.yearqtr(transaction_date, format = "%Y-%m-%d"), "%Y Q%q")) %>%
           ggplot(aes(x = quarter_year, fill = type)) + 
           geom_histogram(aes(fill=type), stat = "count", color = "black") + 
-          scale_fill_manual(values = c("buy" = "#00BA38", "sell" = "#F8766D")) +
+          scale_fill_manual(values = c("buy" = "#01FF70", "sell" = "#DD4B39")) +
           scale_x_discrete(limits = unique(gov_trades$quarter_year)) + 
           labs(x=element_blank(), y = element_blank()) + 
           ggtitle("Trade Activity") +
-          theme(panel.background = element_rect(fill = "white"), panel.grid.minor = element_blank(), panel.grid.major = element_blank()) +
-          geom_text(stat='count', aes(label=..count..),position=position_stack(vjust=0.5))
+          theme(legend.background = element_rect(fill = "#ECF0F5"),
+                panel.background = element_rect(fill = "#ECF0F5"),
+                plot.background = element_rect(fill = "#ECF0F5"),
+                panel.grid.minor = element_blank(), 
+                panel.grid.major = element_blank()) +
+          geom_text(stat='count', aes(label=after_stat(count)),position=position_stack(vjust=0.5))
         )
     
+    output$buy_perf <- renderPlot(
+      gov_trades %>%
+        filter((state == input$stateinputid) & (representative == input$repsinputid) & (type == "buy")) %>%
+        mutate(price_performance_one_month = (price_one_month-price)/price,
+               price_performance_three_months = (price_three_months-price)/price,
+               price_performance_one_year = (price_one_year-price)/price,
+               voo_performance_one_month = (voo_one_month-voo)/price,
+               voo_performance_three_months = (voo_three_months-voo)/price,
+               voo_performance_one_year = (voo_one_year-voo)/price) %>%
+        summarise(type = "buy",
+                  gov_short = mean(price_performance_one_month, na.rm = TRUE),
+                  voo_short = mean(voo_performance_one_month, na.rm = TRUE),
+                  gov_med = mean(price_performance_three_months, na.rm = TRUE),
+                  voo_med = mean(voo_performance_three_months, na.rm = TRUE),
+                  gov_long = mean(price_performance_one_year, na.rm = TRUE),
+                  voo_long = mean(voo_performance_one_year, na.rm = TRUE)) %>%
+        pivot_longer(cols = 2:7, names_to = "trader", values_to = "values") %>% 
+        mutate(trader_cat = ifelse(startsWith(trader, "gov"), "Government", "S&P 500"),
+               type_cat = case_when(endsWith(trader, "short") ~ "Short",
+                                    endsWith(trader, "med") ~ "Medium",
+                                    endsWith(trader, "long") ~ "Long")) %>%
+        group_by(trader_cat) %>%
+        summarise(values = mean(values)) %>%
+        mutate(rank = rank(-values),
+               winner = ifelse(rank == 1,"Winner", "Loser")) %>%
+        ggplot(aes(x=trader_cat, y=values, fill = winner)) + 
+        geom_col(aes(linewidth = 4)) +
+        scale_y_continuous(labels = scales::percent) +
+        labs(x=element_blank(), y = element_blank()) + 
+        ggtitle("Buy Timing Performance") +
+        geom_text(aes(label = paste0(round(values * 100, 2), "%")), 
+                  vjust = -1) +
+        theme(legend.background = element_rect(fill = "#ECF0F5"),
+              panel.background = element_rect(fill = "#ECF0F5"),
+              plot.background = element_rect(fill = "#ECF0F5"),
+              panel.grid.minor = element_blank(), 
+              panel.grid.major = element_blank(),
+              legend.position = "none") +
+        scale_fill_manual(values = pal)
+      )
+    
+    output$sell_perf <- renderPlot(
+      gov_trades %>%
+        filter((state == input$stateinputid) & (representative == input$repsinputid) & (type == "sell")) %>%
+        mutate(price_performance_one_month = (price_one_month-price)/price,
+               price_performance_three_months = (price_three_months-price)/price,
+               price_performance_one_year = (price_one_year-price)/price,
+               voo_performance_one_month = (voo_one_month-voo)/price,
+               voo_performance_three_months = (voo_three_months-voo)/price,
+               voo_performance_one_year = (voo_one_year-voo)/price) %>%
+        summarise(type = "sell",
+                  gov_short = mean(price_performance_one_month, na.rm = TRUE),
+                  voo_short = mean(voo_performance_one_month, na.rm = TRUE),
+                  gov_med = mean(price_performance_three_months, na.rm = TRUE),
+                  voo_med = mean(voo_performance_three_months, na.rm = TRUE),
+                  gov_long = mean(price_performance_one_year, na.rm = TRUE),
+                  voo_long = mean(voo_performance_one_year, na.rm = TRUE)) %>%
+        pivot_longer(cols = 2:7, names_to = "trader", values_to = "values") %>% 
+        mutate(trader_cat = ifelse(startsWith(trader, "gov"), "Government", "S&P 500"),
+               type_cat = case_when(endsWith(trader, "short") ~ "Short",
+                                    endsWith(trader, "med") ~ "Medium",
+                                    endsWith(trader, "long") ~ "Long")) %>%
+        group_by(trader_cat) %>%
+        summarise(values = mean(values)) %>%
+        mutate(rank = rank(values),
+               winner = ifelse(rank == 1,"Winner", "Loser")) %>%
+        ggplot(aes(x=trader_cat, y=values, fill = winner)) + 
+        geom_col(aes(linewidth = 4)) +
+        scale_y_continuous(labels = scales::percent) +
+        labs(x=element_blank(), y = element_blank()) + 
+        ggtitle("Sell Timing Performance") +
+        geom_text(aes(label = paste0(round(values * 100, 2), "%")), 
+                  vjust = -1) +
+        theme(legend.background = element_rect(fill = "#ECF0F5"),
+              panel.background = element_rect(fill = "#ECF0F5"),
+              plot.background = element_rect(fill = "#ECF0F5"),
+              panel.grid.minor = element_blank(), 
+              panel.grid.major = element_blank(),
+              legend.position = "none") +
+        scale_fill_manual(values = pal)
+    )
+    
+    
 
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 # Return a Shiny app object
