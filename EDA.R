0d7e0866611b2415ce3fc6d1b9b18073b3d75a4f
schmeeed <Schmidt5364@gmail.com>
2023-01-31 23:31:29 -0500

    continued EDA for shiny app

--- EDA.R
+++ EDA.R
@@ -3,43 +3,42 @@ library(ggplot2)
 library(lubridate)
 library(tidyr)
 
-
+getwd()
 gov_trades <- read.csv("data/CLEAN-gov-trades.csv", header = TRUE, sep = ",")
-report_days <- read.csv("data/CLEAN-report-days.csv", header = TRUE, sep = ",")
-committee_2020 <- read.csv("data/CLEAN-committee_2020.csv", header = TRUE, sep = ",")
-committee_2021 <- read.csv("data/CLEAN-committee_2021.csv", header = TRUE, sep = ",")
-committee_2022 <- read.csv("data/CLEAN-committee_2022.csv", header = TRUE, sep = ",")
+report_days <- read.csv("data/CLEAN-gov-report-days.csv", header = TRUE, sep = ",")
+committee_2020 <- read.csv("data/CLEAN-committee-2020-data.csv", header = TRUE, sep = ",")
+committee_2021 <- read.csv("data/CLEAN-committee-2021-data.csv", header = TRUE, sep = ",")
+committee_2022 <- read.csv("data/CLEAN-committee-2022-data.csv", header = TRUE, sep = ",")
 
-colnames(gov_trades)
+gov_trades <- gov_trades %>% mutate(disclosure_date = as.Date(disclosure_date),
+                      transaction_date = as.Date(transaction_date))
 
+summary(gov_trades)
 
-gov_trades %>% filter(is.na(type))
 ####AVERAGE DELAY BETWEEN REPORTING AND TRADING#####
 gov_trades %>%
   mutate(delay = disclosure_date - transaction_date) %>%
+  filter(delay > 0) %>%
   summarise(average_delay = mean(delay), max_delay = max(delay), min_delay = min(delay))
+
 #### Buy performance vs Sell Performance ####
-unique(gov_trades$type)
 gov_trades <- gov_trades %>% mutate(type = ifelse(type %in% c("sale_partial", "sale_full", "sale"), "sell", type),
                                     type = ifelse(type=="purchase", "buy", type))
 #BUY
 gov_trades %>%
   mutate(buy_performance_one_month = ifelse(type == "buy", price_one_month-price, NA),
          buy_performance_three_months = ifelse(type == "buy", price_three_months-price, NA),
-         buy_performance_one_year = ifelse(type == "buy", price_one_year-price, NA),
-         buy_performance_avg = mean(c(buy_performance_one_month, buy_performance_three_months, buy_performance_one_year), na.rm = TRUE)) %>%
-  select(representative, buy_performance_one_month, buy_performance_three_months, buy_performance_one_year, buy_performance_avg)
+         buy_performance_one_year = ifelse(type == "buy", price_one_year-price, NA)) %>%
+  select(representative, buy_performance_one_month, buy_performance_three_months, buy_performance_one_year)
 
 gov_trades %>%
   filter(type == "sell") %>%
   mutate(sell_performance_one_month = price_one_month-price,
          sell_performance_three_months = price_three_months-price,
-         sell_performance_one_year = price_one_year-price,
-         sell_performance_avg = (sell_performance_one_month + sell_performance_three_months + sell_performance_one_year)/3) %>%
+         sell_performance_one_year = price_one_year-price) %>%
   select(representative, sell_performance_one_month, sell_performance_three_months, sell_performance_one_year, sell_performance_avg)
 
 
-sum(c(3, NA, 5),na.rm = TRUE)
 ####percentage gain/loss####
 install.packages("scales")
 library(scales)
@@ -283,7 +282,9 @@ economic_indic <- read.csv("https://brian-ralston-r-project.s3.amazonaws.com/eco
           skip = 9, header = TRUE)
 
 
-##########################
+
+#### Trade Activity - APP ####
+
 library(zoo)
 library(scales)
 
@@ -302,9 +303,162 @@ gov_trades %>%
   geom_text(stat='count', aes(label=..count..),position=position_stack(vjust=0.5))
 
 
+#### Trader Performance - APP ####
+
+buy_perf_avg <- gov_trades %>%
+  mutate(price_performance_one_month = (price_one_month-price)/price,
+         price_performance_three_months = (price_three_months-price)/price,
+         price_performance_one_year = (price_one_year-price)/price,
+         voo_performance_one_month = (voo_one_month-voo)/price,
+         voo_performance_three_months = (voo_three_months-voo)/price,
+         voo_performance_one_year = (voo_one_year-voo)/price) %>%
+  filter(representative == "Nancy Pelosi", type == "buy") %>%
+  summarise(type = "buy",
+            gov_short = mean(price_performance_one_month, na.rm = TRUE),
+            voo_short = mean(voo_performance_one_month, na.rm = TRUE),
+            gov_med = mean(price_performance_three_months, na.rm = TRUE),
+            voo_med = mean(voo_performance_three_months, na.rm = TRUE),
+            gov_long = mean(price_performance_one_year, na.rm = TRUE),
+            voo_long = mean(voo_performance_one_year, na.rm = TRUE)) %>%
+  pivot_longer(cols = 2:7, names_to = "trader", values_to = "values") %>% 
+  mutate(trader_cat = ifelse(startsWith(trader, "gov"), "Government", "S&P 500"),
+         type_cat = case_when(endsWith(trader, "short") ~ "Short",
+                              endsWith(trader, "med") ~ "Medium",
+                              endsWith(trader, "long") ~ "Long"))
+
+sell_perf_avg <- gov_trades %>%
+  mutate(price_performance_one_month = (price_one_month-price)/price,
+         price_performance_three_months = (price_three_months-price)/price,
+         price_performance_one_year = (price_one_year-price)/price,
+         voo_performance_one_month = (voo_one_month-voo)/price,
+         voo_performance_three_months = (voo_three_months-voo)/price,
+         voo_performance_one_year = (voo_one_year-voo)/price) %>%
+  filter(representative == "Nancy Pelosi", type == "sell") %>%
+  summarise(type = "sell",
+            gov_short = mean(price_performance_one_month, na.rm = TRUE),
+            voo_short = mean(voo_performance_one_month, na.rm = TRUE),
+            gov_med = mean(price_performance_three_months, na.rm = TRUE),
+            voo_med = mean(voo_performance_three_months, na.rm = TRUE),
+            gov_long = mean(price_performance_one_year, na.rm = TRUE),
+            voo_long = mean(voo_performance_one_year, na.rm = TRUE)) %>%
+  pivot_longer(cols = 2:7, names_to = "trader", values_to = "values") %>% 
+  mutate(trader_cat = ifelse(startsWith(trader, "gov"), "Government", "S&P 500"),
+         type_cat = case_when(endsWith(trader, "short") ~ "Short",
+                              endsWith(trader, "med") ~ "Medium",
+                              endsWith(trader, "long") ~ "Long"))
+
+buy_sell_perf_avg <- bind_rows(buy_perf_avg, sell_perf_avg)
+buy_sell_perf_avg
+
+# visuals
+pal <- c("Winner" = "#01FF70",
+         "Loser" = "grey")
+
+buy_perf_avg %>% 
+  group_by(trader_cat) %>%
+  summarise(values = mean(values)) %>%
+  mutate(rank = rank(-values),
+         winner = ifelse(rank == 1,"Winner", "Loser")) %>%
+  ggplot(aes(x=trader_cat, y=values, fill = winner)) + 
+  geom_col(aes(size = 4)) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(x=element_blank(), y = element_blank()) + 
+  ggtitle("Buy Timing Performance") +
+  geom_text(aes(label = paste0(round(values * 100, 2), "%")), 
+            vjust = -1) +
+  theme(legend.background = element_rect(fill = "#ECF0F5"),
+      panel.background = element_rect(fill = "#ECF0F5"),
+      plot.background = element_rect(fill = "#ECF0F5"),
+      panel.grid.minor = element_blank(), 
+      panel.grid.major = element_blank(),
+      legend.position = "none") +
+  scale_fill_manual(values = pal)
+
+sell_perf_avg %>% 
+  group_by(trader_cat) %>%
+  summarise(values = mean(values)) %>%
+  mutate(rank = rank(values),
+         winner = ifelse(rank == 1,"Winner", "Loser")) %>%
+  ggplot(aes(x=trader_cat, y=values, fill = winner)) + 
+  geom_col(aes(size = 4)) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(x=element_blank(), y = element_blank()) + 
+  ggtitle("Sell Timing Performance") +
+  geom_text(aes(label = paste0(round(values * 100, 2), "%")), 
+            vjust = -1) +
+  theme(legend.background = element_rect(fill = "#ECF0F5"),
+        panel.background = element_rect(fill = "#ECF0F5"),
+        plot.background = element_rect(fill = "#ECF0F5"),
+        panel.grid.minor = element_blank(), 
+        panel.grid.major = element_blank(),
+        legend.position = "none") +
+  scale_fill_manual(values = pal)
+
+buy_sell_perf_avg %>% 
+  ggplot(aes(x=type, y=values , fill = trader_cat)) +
+  geom_col(position = "dodge") +
+  facet_grid(cols = vars(type_cat))
+
+
+gov_trades %>%
+  filter(representative == "Nancy Pelosi") %>%
+  mutate(transaction_date = as.Date(transaction_date)) %>%
+  arrange(transaction_date) %>%
+  mutate(quarter_year = format(as.yearqtr(transaction_date, format = "%Y-%m-%d"), "%Y Q%q")) %>%
+  ggplot(aes(x = quarter_year, fill = type)) + 
+  geom_histogram(aes(fill=type), stat = "count", color = "black") + 
+  scale_fill_manual(values = c("buy" = "#00BA38", "sell" = "#F8766D")) +
+  scale_x_discrete(limits = unique(gov_trades$quarter_year)) + 
+  labs(x=element_blank(), y = element_blank()) + 
+  ggtitle("Trade Activity") +
+  theme(legend.background = element_rect(fill = "#ECF0F5"),
+        panel.background = element_rect(fill = "#ECF0F5"),
+        plot.background = element_rect(fill = "#ECF0F5"),
+        panel.grid.minor = element_blank(), 
+        panel.grid.major = element_blank()) +
+  geom_text(stat='count', aes(label=..count..),position=position_stack(vjust=0.5))
 
 
 
+
+gov_trades %>%
+  filter((state == "CT") & (representative == "Joe Courtney")) %>%
+  mutate(price_performance_one_month = (price_one_month-price)/price,
+         price_performance_three_months = (price_three_months-price)/price,
+         price_performance_one_year = (price_one_year-price)/price,
+         voo_performance_one_month = (voo_one_month-voo)/price,
+         voo_performance_three_months = (voo_three_months-voo)/price,
+         voo_performance_one_year = (voo_one_year-voo)/price) %>%
+  summarise(type = "sell",
+            gov_short = mean(price_performance_one_month, na.rm = TRUE),
+            voo_short = mean(voo_performance_one_month, na.rm = TRUE),
+            gov_med = mean(price_performance_three_months, na.rm = TRUE),
+            voo_med = mean(voo_performance_three_months, na.rm = TRUE),
+            gov_long = mean(price_performance_one_year, na.rm = TRUE),
+            voo_long = mean(voo_performance_one_year, na.rm = TRUE)) %>%
+  pivot_longer(cols = 2:7, names_to = "trader", values_to = "values") %>% 
+  mutate(trader_cat = ifelse(startsWith(trader, "gov"), "Government", "S&P 500"),
+         type_cat = case_when(endsWith(trader, "short") ~ "Short",
+                              endsWith(trader, "med") ~ "Medium",
+                              endsWith(trader, "long") ~ "Long")) %>%
+  group_by(trader_cat) %>%
+  summarise(values = mean(values)) %>%
+  mutate(rank = rank(values),
+         winner = ifelse(rank == 1,"Winner", "Loser")) %>%
+  ggplot(aes(x=trader_cat, y=values, fill = winner)) + 
+  geom_col(aes(linewidth = 4)) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(x=element_blank(), y = element_blank()) + 
+  ggtitle("Sell Timing Performance") +
+  geom_text(aes(label = paste0(round(values * 100, 2), "%")), 
+            vjust = -1) +
+  theme(legend.background = element_rect(fill = "#ECF0F5"),
+        panel.background = element_rect(fill = "#ECF0F5"),
+        plot.background = element_rect(fill = "#ECF0F5"),
+        panel.grid.minor = element_blank(), 
+        panel.grid.major = element_blank(),
+        legend.position = "none") +
+  scale_fill_manual(values = pal)
 
 
 
